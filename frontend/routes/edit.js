4201ccdd6ec55014da373417ebef63a423cc6590
TCK1997 <achinkhang97.sg@gmail.com>
2021-08-06 20:06:22 +0800

    Change 127.0.0.1 to localhost

--- frontend/routes/edit.js
+++ frontend/routes/edit.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET item listing. */
 router.get('/', function(req, res, next) {
-  axios.get('http://127.0.0.1:8000/api/items/')
+  axios.get('http://localhost:8000/api/items/')
       .then(response=>{
         res.render("edit", {title:"Wok Way", data: response.data})
       }).catch(
@@ -18,9 +18,9 @@ router.post('/add', function (req, res) {
   console.log("ADD")
   const data = {name: req.body.addName, price: req.body.addPrice}
   console.log(data)
-  axios.post('http://127.0.0.1:8000/api/items/', data)
+  axios.post('http://localhost:8000/api/items/', data)
   .then(reponse=>{
-    axios.get('http://127.0.0.1:8000/api/items/')
+    axios.get('http://localhost:8000/api/items/')
     .then(response=>{
       res.render("edit", {title:"Wok Way", data: response.data})
     }).catch(
@@ -40,9 +40,9 @@ router.post('/update', function (req, res) {
   const data = {id: req.body.itemId, name: req.body.updateName, price: req.body.updatePrice}
   console.log(data)
 
-  axios.put('http://127.0.0.1:8000/api/items/' + data.id + '/', data)
+  axios.put('http://localhost:8000/api/items/' + data.id + '/', data)
   .then(reponse=>{
-    axios.get('http://127.0.0.1:8000/api/items/')
+    axios.get('http://localhost:8000/api/items/')
     .then(response=>{
       res.render("edit", {title:"Wok Way", data: response.data})
     }).catch(
@@ -62,9 +62,9 @@ router.post('/delete', function (req, res) {
   console.log("DELETE")
   const data = {id: req.body.itemId}
   console.log(data)
-  axios.delete('http://127.0.0.1:8000/api/items/' + data.id + '/', data)
+  axios.delete('http://localhost:8000/api/items/' + data.id + '/', data)
   .then(reponse=>{
-    axios.get('http://127.0.0.1:8000/api/items/')
+    axios.get('http://localhost:8000/api/items/')
     .then(response=>{
       // handle success
       res.render("edit", {title:"Wok Way", data: response.data})
